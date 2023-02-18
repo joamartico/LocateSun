c974469b98bc811c2100e2d6fc7ffd502db1144a
Joa Marticorena <joamartico@gmail.com>
2023-02-18 13:10:35 -0300

    buildExcludes, skipWaiting and runtimeCaching

--- next.config.js
+++ next.config.js
@@ -1,16 +1,20 @@
 /** @type {import('next').NextConfig} */
+const runtimeCaching = require("next-pwa/cache");
 
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  register:true,
-  // disable: process.env.NODE_ENV === 'development'
-})
+const withPWA = require("next-pwa")({
+	dest: "public",
+	register: true,
+	skipWaiting: true,
+	runtimeCaching,
+	buildExcludes: [/middleware-manifest.json$/],
+	// disable: process.env.NODE_ENV === 'development'
+});
 const nextConfig = withPWA({
-  reactStrictMode: true,
-  swcMinify: true,
-  // images: {
-  //   domains:["images.ctfassets.net"]
-  // }
-})
+	reactStrictMode: true,
+	swcMinify: true,
+	// images: {
+	//   domains:["images.ctfassets.net"]
+	// }
+});
 
-module.exports = nextConfig
+module.exports = nextConfig;
